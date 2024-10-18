fcac1b9ff90ccdf0b1450f9980d1f06cbe970cb3
TSH-19 <tomas.santos.hilario@gmail.com>
2024-10-19 00:36:32 +0100

    ...

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 https://medium.com/@wujido20/handling-flags-in-bash-scripts-4b06b4d0ed04
 https://linuxize.com/post/bash-functions/
 https://www.uptimia.com/questions/how-to-compare-file-dates-in-bash
+https://superuser.com/questions/352289/bash-scripting-test-for-empty-directory
 
--- rm_old_files.sh
+++ rm_old_files.sh
@@ -9,18 +9,20 @@ rm_old_files(){
     dir_backup="$2"
     checking="$3"
 
-    for file in "$dir_backup"/*; do
-        
-        fname="${file##*/}"
+    if ! [ -z "$( ls -A $dir_backup )" ]; then          # garante q o dir n está vazio
+        for file in "$dir_backup"/*; do
+            fname="${file##*/}"
 
-        if ! [ -e "$fname" ]; then        # verifica se o ficherio ainda existe no dir de trabalho
-            
-            if $checking; then
-                echo "rm $dir_backup/$fname fff"          # printa os comandos estando no modo checking
-            else
-                rm "$dir_backup/$fname"              # executa os comandos não estando no modo checking
+            if ! [ -e "dir_trabalho/$fname" ]; then        # verifica se o ficherio ainda existe no dir de trabalho
+                
+                if $checking; then
+                    echo "rm $dir_backup/$fname"          # printa os comandos estando no modo checking
+                else
+                    rm "$dir_backup/$fname"              # executa os comandos não estando no modo checking
+                fi
             fi
-        fi
 
-    done
+        done
+    fi
+
 }
