096277d3b9d0471c7fb9acaa2fc94f7eaadba061
Diogo Duarte <duarte.diogohenrique@gmail.com>
2024-10-19 11:27:44 +0100

    Adicionei comentários

--- backup_files.sh
+++ backup_files.sh
@@ -3,20 +3,20 @@
 
 checking=false
 
-while getopts "c" option; do
+while getopts "c" option; do                                # itera sobre as opções passadas na linha de comandos e armazena em option 
     case $option in
         c)
-            checking=true
+            checking=true                                   # modo checking 
             ;;
         *)
-            echo "Usage: $0 [-c] dir_trabalho dir_backup"
+            echo "Usage: $0 [-c] dir_trabalho dir_backup"   # modo de execução
             exit
             ;;
     esac
 done
 
 #validação dos argumentos:
-shift $((OPTIND - 1))
+shift $((OPTIND - 1))                                       # remove os argumentos iterados no loop anterior ou seja fica só com os diretorios
 dir_trabalho="$1"
 dir_backup="$2"
 
@@ -26,21 +26,21 @@ if [ $# -gt 2 ] || ! [ -d $dir_trabalho ] || ! [ -d $dir_backup ]; then
     exit    
 fi
 
-rm_old_files $dir_trabalho $dir_backup $checking
+rm_old_files $dir_trabalho $dir_backup $checking            # remove os ficheiros que já não estou no dir_trabalho da backup
 
 for file in "$dir_trabalho"/*; do
         
-    fname="${file##*/}"
+    fname="${file##*/}"                         # remove o prefixo do caminho deixando apenas o nome do arquivo
 
     if [ -e "$dir_backup/$fname" ]; then        # verifica se existe no diretório de backup um ficheiro com o mesmo nome
         backed_file=$dir_backup/$fname
-        if [ $fname -nt $backed_file ]; then    # ve se o ficheiro no dir trabalho é mais recente que o ficherio com o mesmo nome no dir de backup
+        if [ $fname -nt $backed_file ]; then    # ve se o ficheiro no dir trabalho é mais recente que o ficheiro com o mesmo nome no dir de backup || nt--> newer than
             if $checking; then
                 echo "rm $backed_file"          # printa os comandos estando no modo checking
                 echo "cp -a $fname $dir_backup"
             else
                 rm "$backed_file"               # executa os comandos não estando no modo checking
-                cp -a "$fname" "$dir_backup"
+                cp -a "$fname" "$dir_backup"    # -a preserva permissões/data modificação
             fi
         fi
 
@@ -48,7 +48,7 @@ for file in "$dir_trabalho"/*; do
         if $checking; then
             echo "cp -a $fname $dir_backup"     # printa os comandos estando no modo checking
         else
-            cp -a "$fname" "$dir_backup"          # executa os comandos não estando no modo checking
+            cp -a "$fname" "$dir_backup"        # executa os comandos não estando no modo checking
         fi
     fi
 
